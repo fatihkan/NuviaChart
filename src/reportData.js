f2044b556ec6890144872a894293f9e4363a8fe9
fatih <fatihkan@hotmail.com.tr>
2023-05-04 09:41:03 +0000

    Update reportData.js

--- src/reportData.js
+++ src/reportData.js
@@ -34,6 +34,7 @@ const ReportData = (defaultJson) => {
       // last 24 saat durumunda burda kontrol edilmesi gerekli
       if (
         item.time.includes("00:00:00.000Z") ||
+        item.time.includes("23:59:59.000Z") ||
         item.time.includes("23:59:59.999Z")
       ) {
         item.time = dayjs(item.time).subtract(3, "hour");
